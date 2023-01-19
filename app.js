79cf1132f864163764c21900f9cb93ecc32b2185
Mehrab093 <mehrab093@gmail.com>
2023-01-19 23:08:33 +0330

    first commit

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
